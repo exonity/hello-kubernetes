a1005e7762a3ac13e2ecbb2485f7810f3cb23d9b
markus <mangei.markus@googlemail.com>
2020-07-03 11:28:05 +0200

    initial commit

---
+++ src/main/java/de/mangei/hellokubernetes/HelloKubernetesApplication.java
@@ -0,0 +1,13 @@
+package de.mangei.hellokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mangei/hellokubernetes/HelloKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mangei.hellokubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
